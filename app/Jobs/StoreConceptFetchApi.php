2eda4533efa6344e5d74aae1d18053aa57524286
patrickDigits <patrickpunzalan@digits.ph>
2023-06-06 13:52:42 +0800

    storeconceptapi

---
+++ app/Jobs/StoreConceptFetchApi.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Jobs;
+
+use App\StoreConcept;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class StoreConceptFetchApi implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        // Localhost fetch campaign
+		$response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+        ])->post('http://127.0.0.1:1000/api/get-token', [
+            'secret' => '9384c81fb1f9e661946976585fb0d75a',
+        ]);
+
+        $get_token = $response->json('data.access_token');
+
+        $redemption_list = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
+        ])->get('http://127.0.0.1:1000/api/store_concept');
+
+        $gc_list_fetch = $redemption_list->json();
+        
+        $gc_list_data = array_reverse($gc_list_fetch['data']);
+
+        foreach ($gc_list_data as $item) {
+            StoreConcept::updateOrCreate(
+                ['id' => $item['id']],
+                $item
+            );
+        }
+    }
+}
