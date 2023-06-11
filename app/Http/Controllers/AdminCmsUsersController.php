c4bb15b6488cac8bf64ef09b8d9d510863fff409
patrickDigits <patrickpunzalan@digits.ph>
2023-06-11 19:27:43 +0800

    add script in cms users

--- app/Http/Controllers/AdminCmsUsersController.php
+++ app/Http/Controllers/AdminCmsUsersController.php
@@ -1,13 +1,15 @@
 <?php namespace App\Http\Controllers;
 
+use App\CompanyId;
+use App\Jobs\CompanyIdsJob;
 use Session;
 use Request;
 use DB;
 use CRUDbooster;
 use crocodicstudio\crudbooster\controllers\CBController;
+use Illuminate\Support\Facades\Http;
 
 class AdminCmsUsersController extends CBController {
-
 
 	public function cbInit() {
 		# START CONFIGURATION DO NOT REMOVE THIS LINE
@@ -33,12 +35,31 @@ class AdminCmsUsersController extends CBController {
 		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.CRUDBooster::getCurrentId());		
 		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000','resize_width'=>90,'resize_height'=>90);											
 		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);						
-		$this->form[] = array("label"=>"Store Concept","name"=>"store_concept","type"=>"select","datatable"=>"store_concepts,name",'required'=>true);						
+		$this->form[] = array("label"=>"Company Name","name"=>"company_id","type"=>"select","datatable"=>"company_ids,company_name",'required'=>true);						
 		// $this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
 		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
 		$this->form[] = array("label"=>"Password Confirmation","name"=>"password_confirmation","type"=>"password","help"=>"Please leave empty if not change");
 		# END FORM DO NOT REMOVE THIS LINE
+
+		$this->script_js = "
+		
+			$(document).ready(function(){
+
+				$('#form-group-company_id').hide();
 				
+				$('#id_cms_privileges').on('change', function(){
+
+					const company_id = $(this).val();
+					if(company_id == 2){
+						$('#form-group-company_id').show();
+						$('#company_id').attr('required', true);
+					}else{
+						$('#form-group-company_id').hide();
+						$('#company_id').attr('required', false);
+					}
+				});
+			});
+		";
 	}
 
 	public function getProfile() {			
