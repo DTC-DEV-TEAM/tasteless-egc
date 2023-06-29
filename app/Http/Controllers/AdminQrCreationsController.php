04176f65557d51d85c1cdf8c0c45c9df3c246334
patrickDigits <patrickpunzalan@digits.ph>
2023-06-29 17:42:53 +0800

    change name

--- app/Http/Controllers/AdminQrCreationsController.php
+++ app/Http/Controllers/AdminQrCreationsController.php
@@ -73,11 +73,10 @@ class AdminQrCreationsController extends \crocodicstudio\crudbooster\controllers
 			$this->form[] = ['label'=>'Campaign Id','name'=>'campaign_id','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-6'];
 			$this->form[] = ['label'=>'Gc Description','name'=>'gc_description','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-6'];
 			$this->form[] = ['label'=>'Gc Value','name'=>'gc_value','type'=>'number','validation'=>'required|integer|min:0','width'=>'col-sm-6'];
-			$this->form[] = ['label'=>'Number Of Gcs','name'=>'number_of_gcs','type'=>'number','validation'=>'required|integer|min:0','width'=>'col-sm-6'];
+			$this->form[] = ['label'=>'Number Of Gcs','name'=>'batch_number','type'=>'number','validation'=>'required|integer|min:0','width'=>'col-sm-6'];
 			$this->form[] = ['label'=>'Batch Group','name'=>'batch_group','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-6'];
 			// $this->form[] = ['label'=>'Batch Number','name'=>'batch_number','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-6'];
 			$this->form[] = ['label'=>'Company Name','name'=>'company_id','type'=>'select','validation'=>'required|min:1|max:255',"datatable"=>"company_ids,company_name",'width'=>'col-sm-6'];
-			$this->form[] = ['label'=>'Upload Limit','name'=>'upload_limit','type'=>'text','validation'=>'required|min:0','width'=>'col-sm-6'];
 
 
 			# END FORM DO NOT REMOVE THIS LINE
