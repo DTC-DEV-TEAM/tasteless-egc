51c7a18717fe7941be9a9697ee2b4444764dd0c5
patrickDigits <patrickpunzalan@digits.ph>
2023-07-20 11:31:17 +0800

    add run apis

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\RunJobs::class,
+        \App\Console\Commands\RunApis::class,
     ];
 
     /**
@@ -24,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('command:runapis')->dailyAt('00:00');
         $schedule->command('command:runjobs')->everyMinute();
     }
 
