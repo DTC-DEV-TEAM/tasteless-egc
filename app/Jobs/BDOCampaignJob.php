ba2be33a6b6e5da08cad4953553ab3a0fd124891
patrickDigits <patrickpunzalan@digits.ph>
2024-04-02 17:00:34 +0800

    max exceeded

--- app/Jobs/BDOCampaignJob.php
+++ app/Jobs/BDOCampaignJob.php
@@ -9,10 +9,14 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Str;
+use Illuminate\Queue\MaxAttemptsExceededException;
 
 class BDOCampaignJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $tries = 10;
+    public $maxTries = 10;
 
     /**
      * Create a new job instance.
@@ -34,20 +38,24 @@ class BDOCampaignJob implements ShouldQueue
      */
     public function handle()
     {
-        $total_row = $this->gc_list['total_rows'];
-        
-        for ($i = 0; $i < $total_row; $i++) {
-            do {
-                $generated_qr_code = Str::upper(Str::random(7));
-            } while (GCList::where('qr_reference_number', "BDO$generated_qr_code")->exists());
-        
-            GCList::create([
-                'campaign_id' => $this->gc_list['qr_creations']->id,
-                'name' => 'BDO CUSTOMER',
-                'qr_reference_number' => "BDO$generated_qr_code",
-                'is_fetch' => 0,
-                'created_by' => $gc_list['created_by']
-            ]);
+        try{
+            $total_row = $this->gc_list['total_rows'];
+            
+            for ($i = 0; $i < $total_row; $i++) {
+                do {
+                    $generated_qr_code = Str::upper(Str::random(7));
+                } while (GCList::where('qr_reference_number', "BDO$generated_qr_code")->exists());
+            
+                GCList::create([
+                    'campaign_id' => $this->gc_list['qr_creations']->id,
+                    'name' => 'BDO CUSTOMER',
+                    'qr_reference_number' => "BDO$generated_qr_code",
+                    'is_fetch' => 0,
+                    'created_by' => $gc_list['created_by']
+                ]);
+            }
+        }catch(MaxAttemptsExceededException $e){
+            $this->retryUntil(now()->addSeconds(pow(2, $this->attempts())));
         }
     }
     
